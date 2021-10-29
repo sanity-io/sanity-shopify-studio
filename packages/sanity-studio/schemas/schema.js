853db513b69c4d7b538894b42e95e8600d64ac4f
Knut Melvær <knut.melvaer@gmail.com>
2021-10-29 13:56:58 +0200

    Restructure, add inline docs

--- packages/sanity-studio/schemas/schema.js
+++ packages/sanity-studio/schemas/schema.js
@@ -3,18 +3,24 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-// Annotations
+// Rich text annotations used in the block content editor
 import annotationLinkEmail from './annotations/linkEmail'
 import annotationLinkExternal from './annotations/linkExternal'
 import annotationLinkInternal from './annotations/linkInternal'
 import annotationShopify from './annotations/shopify'
 import annotationShopifyMargin from './annotations/shopifyMargin'
-// Documents
+
+// Document types
 import collection from './documents/collection'
 import page from './documents/page'
 import product from './documents/product'
 import productVariant from './documents/productVariant'
-// Objects
+
+// Singleton document types
+import home from './singletons/home'
+import navigation from './singletons/navigation'
+
+// Object types
 import blockImage from './objects/blockImage'
 import blockProduct from './objects/blockProduct'
 import linkExternal from './objects/linkExternal'
@@ -23,11 +29,9 @@ import productOption from './objects/productOption'
 import proxyString from './objects/proxyString'
 import shopifyProduct from './objects/shopifyProduct'
 import shopifyProductVariant from './objects/shopifyProductVariant'
-// Singletons
-import home from './singletons/home'
-import navigation from './singletons/navigation'
 
-// Then we give our schema to the builder and provide the result to Sanity
+
+// Build the schemas and export to the Sanity Studio app
 export default createSchema({
   // We name our schema
   name: 'default',
@@ -40,11 +44,14 @@ export default createSchema({
     annotationLinkInternal,
     annotationShopify,
     annotationShopifyMargin,
-    // Documents
+    // Document types
     collection,
     page,
     product,
     productVariant,
+    // Singleton document types
+    home,
+    navigation,
     // Objects
     blockImage,
     blockProduct,
@@ -54,8 +61,5 @@ export default createSchema({
     proxyString,
     shopifyProduct,
     shopifyProductVariant,
-    // Singletons
-    home,
-    navigation
   ])
 })
