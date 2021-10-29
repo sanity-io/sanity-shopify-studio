509e9aae4bd6fb58b6e72d8d9391b0782c4fcaa3
Knut Melvær <knut.melvaer@gmail.com>
2021-10-29 13:47:46 +0200

    Adds automatic listing of new types, and filters existing

--- packages/sanity-studio/deskStructure.js
+++ packages/sanity-studio/deskStructure.js
@@ -5,6 +5,8 @@ import { navigation } from './desk/navigation'
 import { pages } from './desk/pages'
 import { products } from './desk/products'
 
+// If you add document types to desk structure manually, you can add them to this array to prevent duplicates in the root pane
+const DOCUMENT_TYPES_IN_STRUCTURE = ['page', 'product', 'productVariant', 'home', 'navigation', 'media.tag']
 export default () => {
   // prettier-ignore
   return (
@@ -19,6 +21,8 @@ export default () => {
         debug,
         S.divider(),
         navigation,
+        // Automatically add new document types to the root pane
+        ...S.documentTypeListItems().filter(listItem => !DOCUMENT_TYPES_IN_STRUCTURE.includes(listItem.getId()))
       ])
   )
 }
