70be7f525e774fa241c30565509543f21625be72
Knut Melvær <knut.melvaer@gmail.com>
2021-11-02 23:31:58 +0100

    tweak copy

--- packages/sanity-studio/deskStructure.js
+++ packages/sanity-studio/deskStructure.js
@@ -9,10 +9,10 @@
     },
   * is added to the `parts` array in `/sanity.json`.
   *
-  * Sanity Studio will automatically list document types out of the box.
+  * Sanity Studio automatically lists document types out of the box.
   * With this custom desk structure we achieve things like showing the `home`
   * and `settings` document types as singletons, and grouping product details
-  * and variants for easy access.
+  * and variants for easy editorial access.
   *
   * You can customize this even further as your schemas progress.
   * To learn more about structure builder, visit our docs:
