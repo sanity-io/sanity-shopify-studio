7375a6372ce9007814f1c89fb2ba746b9008aec0
Robin Pyon <robin.pyon@gmail.com>
2021-11-03 17:01:38 +0000

    fix(hydrogen-app): add products provider wrapper to product pages

--- packages/hydrogen-app/src/pages/products/[handle].server.jsx
+++ packages/hydrogen-app/src/pages/products/[handle].server.jsx
@@ -5,6 +5,7 @@ import Layout from '../../components/Layout.client';
 import NotFound from '../../components/NotFound.server';
 import ProductDetails from '../../components/ProductDetails.client';
 import Seo from '../../components/Seo.client';
+import ProductsProvider from '../../contexts/ProductsProvider.client';
 import {PRODUCT} from '../../fragments/product';
 import useSanityQuery from '../../utils/query/useSanityQuery';
 
@@ -32,26 +33,28 @@ export default function Product() {
   const productVariant = product.storefront?.variants?.edges[0];
 
   return (
-    <Layout>
-      <ProductDetails product={product} />
+    <ProductsProvider value={shopifyProducts}>
+      <Layout>
+        <ProductDetails product={product} />
 
-      {/* SEO */}
-      <Seo
-        page={{
-          description: product.seo?.description,
-          image: product.seo?.image,
-          keywords: product.seo?.keywords,
-          product: {
-            availableForSale: productVariant?.node?.availableForSale,
+        {/* SEO */}
+        <Seo
+          page={{
             description: product.seo?.description,
-            price: productVariant?.node?.priceV2,
+            image: product.seo?.image,
+            keywords: product.seo?.keywords,
+            product: {
+              availableForSale: productVariant?.node?.availableForSale,
+              description: product.seo?.description,
+              price: productVariant?.node?.priceV2,
+              title: product.seo?.title,
+            },
             title: product.seo?.title,
-          },
-          title: product.seo?.title,
-          type: 'product',
-        }}
-      />
-    </Layout>
+            type: 'product',
+          }}
+        />
+      </Layout>
+    </ProductsProvider>
   );
 }
 
